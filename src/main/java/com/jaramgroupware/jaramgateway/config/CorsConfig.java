80738a1016637c07f12283138e788ccd9632ec8c
Junser Hwang <hzser123@gmail.com>
2023-02-19 08:02:56 +0900

    [FIX] CORS 필터에서 set cookie header 허용하도록 수정

--- src/main/java/com/jaramgroupware/jaramgateway/config/CorsConfig.java
+++ src/main/java/com/jaramgroupware/jaramgateway/config/CorsConfig.java
@@ -20,7 +20,7 @@ import java.util.Objects;
 @Configuration
 public class CorsConfig {
 
-    private final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Content-Length, Authorization, credential, X-XSRF-TOKEN";
+    private final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Content-Length, Authorization, credential, X-XSRF-TOKEN,set-cookie,access-control-expose-headers";
     private final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
 
     private final String MAX_AGE = "7200"; //2 hours (2 * 60 * 60)
